2776c9fa52552cf922707119eb4add178eb7184d
nishanttomar-creater <83297578+nishanttomar-creater@users.noreply.github.com>
2021-05-17 15:55:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyparser = require("body-parser");
+
+const app=express();
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+
+});
+
+app.post("/",function(req,res){
+  var num = Number(req.body.num1);
+  var nom= Number(req.body.num2);
+  var resu = num/(nom*num);
+    res.send("The answer is"+" "+resu);
+});
+
+
+
+app.listen(3000,function(){
+  console.log("server has started on port 3000");
+});
